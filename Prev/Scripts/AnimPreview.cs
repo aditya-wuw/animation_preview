9e554c7a7b1fdf71b634585fd783fe49d3a0f807
adi <guddumandal605605@gmail.com>
2026-05-09 21:58:54 +0530

    Final

--- Prev/Scripts/AnimPreview.cs
+++ Prev/Scripts/AnimPreview.cs
@@ -1,8 +1,9 @@
 using Godot;
 using System;
 using System.Linq;
+namespace AnimPreview;
 
-public partial class anim_preview : CanvasLayer
+public partial class AnimPreview : CanvasLayer
 {
    const string smugCat = @"
        /\                ______
@@ -22,36 +23,36 @@ public partial class anim_preview : CanvasLayer
    private AnimationPlayer AnimPlayer;
    public override void _Ready()
    {
-	  GD.Print(smugCat);
-	  SearchBar.TextChanged += SearchTerms;
-	  AnimPlayer = Model.GetNode<AnimationPlayer>("AnimationPlayer");
-	  Anims = AnimPlayer.GetAnimationList();
-	  DisplayAnimations(Anims);
+      GD.Print(smugCat);
+      SearchBar.TextChanged += SearchTerms;
+      AnimPlayer = Model.GetNode<AnimationPlayer>("AnimationPlayer");
+      Anims = AnimPlayer.GetAnimationList();
+      DisplayAnimations(Anims);
    }
 
    private void DisplayAnimations(string[] displayList)
    {
-	  AnimContainer.GetChildren().OfType<Button>().ToList().ForEach(b => b?.QueueFree());
-	  foreach (string anim in displayList)
-	  {
-		 Button newBTN = BtnScene.Instantiate<Button>();
-		 newBTN.Text = anim;
-		 newBTN.Pressed += () => PlayAnimation(anim);
-		 AnimContainer.AddChild(newBTN);
-	  }
+      AnimContainer.GetChildren().OfType<Button>().ToList().ForEach(b => b?.QueueFree());
+      foreach (string anim in displayList)
+      {
+         Button newBTN = BtnScene.Instantiate<Button>();
+         newBTN.Text = anim;
+         newBTN.Pressed += () => PlayAnimation(anim);
+         AnimContainer.AddChild(newBTN);
+      }
    }
 
 
    private void SearchTerms(string Terms)
    {
-	  GD.Print(Terms);
-	  string[] filtered = [.. Anims.Where(s => s.Contains(Terms, StringComparison.CurrentCultureIgnoreCase))];
-	  DisplayAnimations(filtered);
+      GD.Print(Terms);
+      string[] filtered = [.. Anims.Where(s => s.Contains(Terms, StringComparison.CurrentCultureIgnoreCase))];
+      DisplayAnimations(filtered);
    }
 
    private void PlayAnimation(string anim)
    {
-	  AnimPlayer.Play(anim);
+      AnimPlayer.Play(anim);
    }
 
 
